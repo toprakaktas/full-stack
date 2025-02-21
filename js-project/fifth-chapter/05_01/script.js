c748d975c1ea25171619ab35c73792854564ee71
Toprak <topraktas19@hotmail.com>
2025-02-21 22:24:53 +0300

    data types

---
+++ js-project/fifth-chapter/05_01/script.js
@@ -0,0 +1,41 @@
+/**
+ * Data Types in JavaScript
+ * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+ */
+
+// String:
+let stringDemo = "A string of text.";
+console.log("String:", stringDemo);
+    
+// Numbers:
+let integerDemo = 4;
+console.log("Integer:", integerDemo);
+
+let floatDemo = 5.6;
+console.log("Floating point number:", floatDemo);
+
+// Boolean:
+let booleanDemo = true;
+console.log("Boolean value:", booleanDemo);
+
+// Null value (nothing):
+let nullDemo = null;
+console.log("Null value:", nullDemo);
+
+// Undefined:
+let undefinedDemo;
+console.log("Undefined:", undefinedDemo);
+
+let undefinedAssignedDemo = undefined;
+console.log("Undefined assigned:", undefinedAssignedDemo);
+
+// Object:
+const objectDemo = {
+    car: "Audi",
+    model: 2025,
+};
+console.log("Object:", objectDemo);
+
+// Array:
+const arrayDemo = ["Intel", "GeForce", "AMD", "MSI"];
+console.log("Array:", arrayDemo);
